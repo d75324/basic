e50747cacabb5402429bdd3e58cef7a27b16ad74
LVO <112906515+d75324@users.noreply.github.com>
2025-03-16 22:22:10 -0300

    static files configuration revised in settings.py

--- mysite_box/basic/views.py
+++ mysite_box/basic/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+# pagina de inicio
+class HomeView(TemplateView):
+    template_name = 'home.html'
